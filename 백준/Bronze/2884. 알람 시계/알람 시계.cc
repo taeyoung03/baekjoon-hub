51f82610570c61f55249351f4206f64bd2b5b362
taeyoung03 <156279794+taeyoung03@users.noreply.github.com>
2024-01-11 16:57:30 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int H, M;
+	cin >> H >> M;
+	if (M >= 45) cout << H << ' ' << M - 45 << endl;
+	else if (H == 0) cout << 23 << ' ' << M + 15 << endl;
+	else cout << H - 1 << ' ' << M + 15 << endl;
+}
