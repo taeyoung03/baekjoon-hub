ffcbdf4c8e3156bd41e75b7b0461396bd722a98d
taeyoung03 <156279794+taeyoung03@users.noreply.github.com>
2024-01-16 17:22:56 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(void) {
+	string str;
+	string croatian[8] = { "c=", "c-","dz=","d-","lj","nj","s=","z=" };
+	cin >> str;
+	for (int i = 0; i < 8; i++) {
+		while (1) {
+			int idx = str.find(croatian[i]);
+			if (idx == string::npos) break;
+			else str.replace(idx, croatian[i].length(), "@");
+		}
+	}
+	cout << str.length() << endl;
+	return 0;
+}
