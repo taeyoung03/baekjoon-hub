6ed4c9b8c585d058102e81ee8071c59a95907d26
taeyoung03 <156279794+taeyoung03@users.noreply.github.com>
2024-02-13 13:38:47 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	cout << pow(2, n);
+	return 0;
+}
