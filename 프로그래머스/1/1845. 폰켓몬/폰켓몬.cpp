58e7df0ebf2b5ebe3d2f42a19789b31b216dc28c
taeyoung03 <156279794+taeyoung03@users.noreply.github.com>
2024-06-24 18:38:56 +0900

    [level 1] Title: 폰켓몬, Time: 0.25 ms, Memory: 4 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <set>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int answer = 0;
+    set<int> s;
+    
+    for(int i=0; i<nums.size(); i++){
+        s.insert(nums[i]);
+    }
+    answer = s.size();
+    if(answer > nums.size()/2){
+        answer = nums.size()/2;
+    }
+    
+    return answer;
+}
