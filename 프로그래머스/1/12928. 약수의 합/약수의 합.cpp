3029b585b270448b3a8e3b4462f58d65cf19bdde
taeyoung03 <156279794+taeyoung03@users.noreply.github.com>
2024-07-02 22:59:15 +0900

    [level 1] Title: 약수의 합, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
@@ -1,14 +1,18 @@
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 int solution(int n) {
     int answer = 0;
     
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= sqrt(n); i++) {
         if (n % i == 0) {
             answer += i;
+            if (i != n / i) {
+                answer += n / i;
+            }
         }
     }
     
