c9ab684fbbb354e218150293aa08e32ee6c8794b
taeyoung03 <156279794+taeyoung03@users.noreply.github.com>
2024-07-07 20:49:03 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -4,18 +4,21 @@
 using namespace std;
 
 int solution(int num) {
-    int answer = 0;
-    long long n = static_cast<long long>(num);
-    for (answer; answer < 500; answer++) {
-        if (n == 1) {
-            return answer;
-        }
-        else if (n % 2 == 0) {
-            n = n / 2;
-        }
-        else {
-            n = n * 3 + 1;
-        }
+    static int answer = 0;
+    static long long n = static_cast<long long>(num);
+    if (n == 1) {
+        return answer;
     }
-    return -1;
+    else if (answer == 500) {
+        return -1;
+    }
+    else if (n % 2 == 0) {
+        n = n / 2;
+        answer++;
+    }
+    else {
+        n = n * 3 + 1;
+        answer++;
+    }
+    return solution(n);
 }
