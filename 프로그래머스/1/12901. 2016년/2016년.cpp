6282fc908ed69fa0a1555f01536c8e5979b40c48
taeyoung03 <156279794+taeyoung03@users.noreply.github.com>
2024-07-07 18:42:33 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.cpp
+++ 프로그래머스/1/12901. 2016년/2016년.cpp
@@ -4,15 +4,14 @@
 using namespace std;
 
 string solution(int a, int b) {
-    vector<int> month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    vector<string> day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+    vector<int> month = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    vector<string> day = { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" };
     
     int sum = -1;
-    for(int i = 0; i < a - 1; i++) {
+    for (int i = 0; i < a - 1; i++) {
         sum += month[i];
     }
     sum += b;
     
-    string answer = day[sum % 7];
-    return answer;
+    return day[sum % 7];
 }
--- 프로그래머스/1/12901. 2016년/README.md
+++ 프로그래머스/1/12901. 2016년/README.md
@@ -1,10 +1,10 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901) 
 
 ### 성능 요약
 
-메모리: 4.16 MB, 시간: 0.01 ms
+메모리: 4.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 30일 23:10:35
+2024년 07월 07일 18:42:30
 
 ### 문제 설명
 
