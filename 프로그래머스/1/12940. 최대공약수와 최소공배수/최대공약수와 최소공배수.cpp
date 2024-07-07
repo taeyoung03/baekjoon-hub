952984edbf0c02dfb89afcb40e5124b60c84bc35
taeyoung03 <156279794+taeyoung03@users.noreply.github.com>
2024-07-07 20:44:44 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -7,9 +7,7 @@ int GCD(int a, int b) {
     if (a % b == 0) {
         return b;
     }
-    else {
-        return GCD(b, a % b);
-    }
+    return GCD(b, a % b);
 }
 
 vector<int> solution(int n, int m) {
