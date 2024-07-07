3f86568454d46cc89561619513cef6f26953d59b
taeyoung03 <156279794+taeyoung03@users.noreply.github.com>
2024-07-07 19:21:24 +0900

    [level 1] Title: 시저 암호, Time: 0.08 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -4,21 +4,14 @@
 using namespace std;
 
 string solution(string s, int n) {
-    string answer = "";
-    
     for (int i = 0; i < s.length(); i++) {
         if (s[i] == ' ') {
             continue;
         }
-        if ('A' <= s[i] && s[i] <= 'Z' && s[i] + n > 'Z') {
-            s[i] -= 26;
-        }
-        else if ('a' <= s[i] && s[i] <= 'z' && s[i] + n > 'z') {
+        if((s[i] <= 'Z' && s[i] + n > 'Z') || (s[i] >= 'a' && s[i] + n > 'z')) {
             s[i] -= 26;
         }
         s[i] += n;
     }
-    answer = s;
-    
-    return answer;
+    return s;
 }
