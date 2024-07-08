fb6d274353bba2ae7876cb0360e6a8d5835f2963
taeyoung03 <156279794+taeyoung03@users.noreply.github.com>
2024-07-08 22:57:18 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -1,26 +1,23 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
     vector<string> answer;
-    string tmp;
     for (int i = 0; i < n; i++) {
+        arr1[i] = arr1[i] | arr2[i];
+        string tmp = "";
         for (int j = 0; j < n; j++) {
-            if (arr1[i] % 2 == 1 || arr2[i] % 2 == 1) {
-                tmp.append("#");
+            if (arr1[i] % 2 == 1) {
+                tmp = "#" + tmp;
             }
             else {
-                tmp.append(" ");
+                tmp = " " + tmp;
             }
-            arr1[i] /= 2;
-            arr2[i] /= 2;
+            arr1[i] = arr1[i] >> 1;
         }
-        reverse(tmp.begin(), tmp.end());
         answer.push_back(tmp);
-        tmp.clear();
     }
     return answer;
 }
