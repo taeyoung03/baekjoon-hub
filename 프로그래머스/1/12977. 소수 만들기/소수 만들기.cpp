6d0a294fc5688266841c7ff6bcf1402fe66d84e5
taeyoung03 <156279794+taeyoung03@users.noreply.github.com>
2024-07-14 21:58:21 +0900

    [level 1] Title: 소수 만들기, Time: 11.80 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -1,13 +1,12 @@
 #include <vector>
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 bool IsPrime(int num) {
     if (num <= 1) {
         return false;
     }
-    for (int i = 2; i <= sqrt(num); i++) {
+    for (int i = 2; i < num; i++) {
         if (num % i == 0) {
             return false;
         }
@@ -17,14 +16,16 @@ bool IsPrime(int num) {
 
 int solution(vector<int> nums) {
     int answer = 0;
+    int sum = 0;
     
     for (int i = 0; i < nums.size() - 2; i++) {
         for (int j = i + 1; j < nums.size() - 1; j++) {
             for (int k = j + 1; k < nums.size(); k++) {
-                int num = nums[i] + nums[j] + nums[k];
-                if (IsPrime(num)) {
+                sum = nums[i] + nums[j] + nums[k];
+                if (IsPrime(sum)) {
                     answer++;
                 }
+                sum = 0;
             }
         }
     }
