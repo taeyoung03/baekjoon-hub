a305c1154ca65fa72f7a70b970f99d533c08f8b3
taeyoung03 <156279794+taeyoung03@users.noreply.github.com>
2024-07-14 22:01:49 +0900

    [level 1] Title: 소수 만들기, Time: 0.83 ms, Memory: 3.65 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -1,12 +1,13 @@
 #include <vector>
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 bool IsPrime(int num) {
     if (num <= 1) {
         return false;
     }
-    for (int i = 2; i < num; i++) {
+    for (int i = 2; i <= sqrt(num); i++) {
         if (num % i == 0) {
             return false;
         }
